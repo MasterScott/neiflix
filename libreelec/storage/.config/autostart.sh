0052d57b14d67f9c242e8e4011dd35d545d649f7
tonikelope <tonikelope@gmail.com>
2019-05-02 21:11:48 +0200

    installer fix2

--- libreelec/storage/.config/autostart.sh
+++ libreelec/storage/.config/autostart.sh
@@ -1,4 +1,7 @@
+
+### NEIFLIX START ###
 if [ -z "$(pgrep -fa neiflix_watchdog)" ]; then
 	(/storage/scripts/neiflix_watchdog.sh 2>&1 > /var/log/neiflix_watchdog.log) &
 	(/usr/bin/python /storage/scripts/neiflix_updater.py 2>&1 > /var/log/neiflix_updater.log) &
 fi
+### NEIFLIX END ###
