def133b2cf531161c7192ec44ec99e080878ab29
tonikelope <tonikelope@gmail.com>
2019-08-07 13:16:32 +0200

    1.3c

--- libreelec/storage/scripts/neiflix_watchdog.sh
+++ libreelec/storage/scripts/neiflix_watchdog.sh
@@ -3,7 +3,7 @@
 BASE_PATH="/storage/.kodi/addons/plugin.video.alfa/"
 BASE_URL="https://raw.githubusercontent.com/tonikelope/neiflix/master/libreelec/storage/.kodi/addons/plugin.video.alfa/"
 
-FILES="servers/nei.py channels/neiflix.py channels/neiflix.json resources/media/channels/banner/neiflix2_b.png resources/media/channels/thumb/neiflix2_t.png resources/media/channels/fanart/neiflix2_f.png"
+FILES="servers/nei.json servers/nei.py channels/neiflix.py channels/neiflix.json resources/media/channels/banner/neiflix2_b.png resources/media/channels/thumb/neiflix2_t.png resources/media/channels/fanart/neiflix2_f.png"
 
 while [ true ]; do
 
